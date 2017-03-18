301dedd8d6aee7d5baff60bd2ca1e2ebfea1f943
Fulir <juraj.fulir@fer.hr>
2017-03-18 23:31:01 +0100

    Add the DialGraph for displaying an angle

---
+++ src/com/lirfu/lirfugraph/DialGraph.java
@@ -0,0 +1,39 @@
+package com.lirfu.lirfugraph;
+
+import java.awt.*;
+
+/**
+ * Created by lirfu on 16.03.17..
+ */
+public class DialGraph extends GraphTemplate {
+    private double angle;
+    private String title;
+
+    public DialGraph(String title, double angle) {
+        this.angle = angle;
+        this.title = title;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Point l = getLocation();
+        Dimension size = getAdjustedSize();
+
+        drawTitleAndFrame(g, title);
+
+        double diameter = Math.min(size.width, size.height);
+        Point center = new Point((int) (l.x + size.width / 2. + padding), (int) (l.y + size.height / 2. + padding));
+
+        // Draw the dial background.
+        g.setColor(interfaceColor);
+        g.fillOval((int) (center.x - diameter / 2.), (int) (center.y - diameter / 2.), (int) diameter, (int) diameter);
+
+        // Draw the dial fence.
+        g.setColor(primaryColor);
+        g.drawOval((int) (center.x - diameter / 2.), (int) (center.y - diameter / 2.), (int) diameter, (int) diameter);
+
+        // Draw the dial.
+        g.setColor(secondaryColor);
+        g.drawLine(center.x, center.y, (int) (center.x + Math.cos(angle) * diameter / 2), (int) (center.y + Math.sin(angle) * diameter / 2));
+    }
+}
